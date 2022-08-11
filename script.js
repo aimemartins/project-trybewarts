e8aea182466078278569ab5a805f8714d5796fff
Marinna Vitoriano <marinna@outlook.com>
2022-08-11 16:20:19 -0300

    requisito 3 finalizado

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
- 
+const email = "tryber@teste.com";
+const senha = "123456";
+const button = document.getElementById("button");
+const inputEmail = document.getElementById("email");
+const inputSenha = document.getElementById("senha");
+
+function validaLogin() {
+    if (inputEmail.value === email && inputSenha.value === senha) {
+        alert("Olá, Tryber!");
+    } else {
+       alert("Email ou senha inválidos.");
+    }
+}
+
+button.addEventListener('click', validaLogin)
