428a702ecf798809bf31ff9d93e0c520c2cad010
Marinna Vitoriano <marinna@outlook.com>
2022-08-11 18:13:37 -0300

    pull

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ const inputEmail = document.getElementById("email");
 const inputSenha = document.getElementById("senha");
 
 function validaLogin() {
-    if (inputEmail.value === email && inputSenha.value === senha) {
-        alert("Olá, Tryber!");
-    } else {
-       alert("Email ou senha inválidos.");
-    }
+  if (inputEmail.value === email && inputSenha.value === senha) {
+    alert("Olá, Tryber!");
+  } else {
+    alert("Email ou senha inválidos.");
+  }
 }
 
 button.addEventListener('click', validaLogin)
