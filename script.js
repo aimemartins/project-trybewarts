dd1242daa7f31cf8a9318159bcb338b703adb072
AIMÊ <aimemartins@hotmail.com>
2022-08-12 11:14:48 -0300

    Requisito 18 finalizado

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ function validaLogin() {
 }
 
 button.addEventListener('click', validaLogin);
+
+// Questão 18 - Habilite o botão "Enviar" após a validação do checkbox
+// Pesquisa feita em:
+// https://cursos.alura.com.br/forum/topico-event-listner-em-um-checkbox-97348
+// https://cursos.alura.com.br/forum/topico-habitar-desabilitar-botao-65202
+
+// desabilita botão no início
+document.getElementById('submit-btn').disabled = true;
+// captura do input (Você concorda com o uso das informações acima?)
+const inputAgreement = document.getElementById('agreement');
+// cria o evento com as mudanças do botão
+inputAgreement.addEventListener('change', validaButton);
+function validaButton() {
+  if (inputAgreement.checked) {
+    document.getElementById('submit-btn').disabled = false;
+  } else {
+    document.getElementById('submit-btn').disabled = true;
+  }
+}
